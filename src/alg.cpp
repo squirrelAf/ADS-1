761b6d2ba326c790c324b34e934e6f8d70886573
squirrelAf <aleckraowo@gmail.com>
2025-04-14 19:19:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,26 +4,67 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value < 2) {
+    return false;
+  }
+
+  for (uint64_t test = 2; test * test <= value; ++test) {
+    if (value % test == 0) {
+      return false;
+    }
+  }
+
   return true;
 }
 
-uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+uint64_t nPrime(uint64_t targetIndex) {
+  uint64_t fo = 0;
+  uint64_t cur = 1;
+
+  while (fo < targetIndex) {
+    ++current;
+    if (checkPrime(cur)) {
+      ++fo;
+    }
+  }
+
+  return cur;
 }
 
-uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+uint64_t nextPrime(uint64_t from) {
+  uint64_t next = from + 1;
+
+  while (!checkPrime(next)) {
+    ++next;
+  }
+
+  return next;
 }
 
-uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+uint64_t sumPrime(uint64_t upperLimit) {
+  uint64_t sum = 0;
+
+  for (uint64_t val = 2; val < upperLimit; ++val) {
+    if (checkPrime(val)) {
+      sum += val;
+    }
+  }
+
+  return sum;
 }
 
-uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+uint64_t twinPrimes(uint64_t lo, uint64_t hi) {
+  uint64_t twinPa = 0;
+  uint64_t lastPri = 0;
+
+  for (uint64_t i = lo; i < hi; ++i) {
+    if (checkPrime(i)) {
+      if (lastPri && i - lastPri == 2) {
+        ++twinPa;
+      }
+      lastPri = i;
+    }
+  }
+
+  return twinPa;
 }
