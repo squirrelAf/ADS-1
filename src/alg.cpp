78eafef2485b4dff8e04a7fd8d5c9cb849ce5c06
squirrelAf <aleckraowo@gmail.com>
2025-04-15 07:54:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,46 +1,36 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-
-
 bool checkPrime(uint64_t value) {
   if (value < 2) {
     return false;
   }
-
   for (uint64_t test = 2; test * test <= value; ++test) {
     if (value % test == 0) {
       return false;
     }
   }
-
   return true;
 }
-
 uint64_t nPrime(uint64_t targetIndex) {
   uint64_t found = 0;
   uint64_t current = 1;
-
   while (found < targetIndex) {
     ++current;
     if (checkPrime(current)) {
       ++found;
     }
   }
-
   return current;
 }
-
 uint64_t nextPrime(uint64_t from) {
   uint64_t next = from + 1;
 
   while (!checkPrime(next)) {
     ++next;
   }
-
   return next;
 }
-
 uint64_t sumPrime(uint64_t upperLimit) {
   uint64_t sum = 0;
 
@@ -49,10 +39,8 @@ uint64_t sumPrime(uint64_t upperLimit) {
       sum += val;
     }
   }
-
   return sum;
 }
-
 uint64_t twinPrimes(uint64_t low, uint64_t high) {
   uint64_t twin = 0;
   uint64_t last = 0;
@@ -68,3 +56,4 @@ uint64_t twinPrimes(uint64_t low, uint64_t high) {
 
   return twin;
 }
+
