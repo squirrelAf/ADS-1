44157e47a897bf411b601da6b64143ccbcc2bf53
squirrelAf <aleckraowo@gmail.com>
2025-04-16 21:59:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,29 +1,56 @@
-// Copyright 2022 NNTU-CS
+
 #include <cstdint>
 #include "alg.h"
-
-
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value < 2) {
+    return false;
+  }
+  for (uint64_t test = 2; test * test <= value; ++test) {
+    if (value % test == 0) {
+      return false;
+    }
+  }
   return true;
 }
-
-uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+uint64_t nPrime(uint64_t targetIndex) {
+  uint64_t found = 0;
+  uint64_t current = 1;
+  while (found < targetIndex) {
+    ++current;
+    if (checkPrime(current)) {
+      ++found;
+    }
+  }
+  return current;
 }
+uint64_t nextPrime(uint64_t from) {
+  uint64_t next = from + 1;
 
-uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  while (!checkPrime(next)) {
+    ++next;
+  }
+  return next;
 }
+uint64_t sumPrime(uint64_t upperLimit) {
+  uint64_t sum = 0;
+  for (uint64_t val = 2; val < upperLimit; ++val) {
+    if (checkPrime(val)) {
+      sum += val;
+    }
+  }
+  return sum;
+}
+uint64_t twinPrimes(uint64_t low, uint64_t high) {
+  uint64_t twinPairs = 0;
+  uint64_t lastPrime = 0;
+  for (uint64_t n = low; n < high; ++n) {
+    if (checkPrime(i)) {
+      if (lastPrime && n - lastPrime == 2) {
+        ++twinPairs;
+      }
+      lastPrime = n;
+    }
+  }
+  return twinPairs;
+} 
 
-uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
-}
-
-uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
-}
